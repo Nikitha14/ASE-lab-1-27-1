e96a4f1a2ed72c0b8328e2a3df5d954cadb2c92e
Nikitha14 <nikitha.nitwarangal@gmail.com>
2016-02-03 22:15:37 -0600

    Commit

---
+++ Source/cam.java
@@ -0,0 +1,89 @@
+package com.example.nikitha.asetutorial;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Im;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import android.graphics.Bitmap;
+import android.os.BatteryManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class cam extends Activity {
+
+    Button btnTakePhoto;
+    ImageView imgTakenPhoto;
+    private static final int CAM_REQUEST = 1313;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam);
+
+        btnTakePhoto = (Button) findViewById(R.id.button1);
+        imgTakenPhoto = (ImageView) findViewById(R.id.imageview1);
+
+        btnTakePhoto.setOnClickListener(new btnTakePhotoClicker());
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CAM_REQUEST)
+        {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imgTakenPhoto.setImageBitmap(thumbnail);
+        }
+
+    }
+
+    class btnTakePhotoClicker implements Button.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            Intent cameraintent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraintent, CAM_REQUEST);
+        }
+
+    }
+}
---
+++ Source/login.java
@@ -0,0 +1,28 @@
+package com.example.nikitha.asetutorial;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class login extends AppCompatActivity {
+
+    EditText etUsername,etPassword;
+    Button bLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+        bLogin = (Button) findViewById(R.id.bLogin);
+
+    }
+}
